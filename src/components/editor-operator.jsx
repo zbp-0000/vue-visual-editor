b36ad46a77edd4b457fb67208882da0f78a2c1a8
Peng <zbp0620@qq.com>
2024-10-31 23:15:57 +0800

    fix: 改bug，修改属性时，元素会返回上一次的位置

--- src/components/editor-operator.jsx
+++ src/components/editor-operator.jsx
@@ -1,6 +1,6 @@
-import {defineComponent, inject, reactive, watch} from "vue";
-import {ElForm, ElFormItem, ElButton, ElInputNumber, ElInput, ElColorPicker, ElSelect, ElOption} from "element-plus";
-import {cloneDeep} from "lodash";
+import { defineComponent, inject, reactive, watch } from "vue";
+import { ElForm, ElFormItem, ElButton, ElInputNumber, ElInput, ElColorPicker, ElSelect, ElOption } from "element-plus";
+import { cloneDeep } from "lodash";
 
 export default defineComponent({
     props: {
@@ -9,32 +9,31 @@ export default defineComponent({
         updateContainer: Function,
         updateBlock: Function
     },
-    setup(props, ctx) {
+    setup (props, ctx) {
         const config = inject('config') // 组件的配置信息
         const state = reactive({
             editData: {}
         })
-        const reset = () => {
-            if(!props.block) { // 说明要绑定的是容器的宽度和高度
+        const reset = (newBlock) => {
+            if (!props.block) { // 说明要绑定的是容器的宽度和高度
                 state.editData = cloneDeep(props.data.container)
             } else {
-                state.editData = cloneDeep(props.block)
+                state.editData = newBlock
             }
         }
         const apply = () => {
-            if(!props.block) { // 更改组件容器的大小
-                props.updateContainer({...props.data, container: state.editData})
+            if (!props.block) { // 更改组件容器的大小
+                props.updateContainer({ ...props.data, container: state.editData })
             } else { // 更改组件的位置
-                console.log("state.editData",state.editData);
-                console.log("props.block",props.block);
+                console.log("state.editData", state.editData);
+                console.log("props.block", props.block);
                 props.updateBlock(state.editData, props.block)
             }
         }
-        watch(() => props.block, reset, {immediate: true})
-        watch(() => props.block, (newBlock) => console.log("newBlock",newBlock))
+        watch(() => props.block, reset, { immediate: true })
         return () => {
             let content = []
-            if(!props.block) {
+            if (!props.block) {
                 content.push(<>
                     <ElFormItem label="容器宽度">
                         <ElInputNumber v-model={state.editData.width}></ElInputNumber>
@@ -45,7 +44,7 @@ export default defineComponent({
                 </>)
             } else {
                 let component = config.componentMap[props.block.key]
-                if(component && component.props) { // {text:{type:'xxx'},size:{},color:{}}
+                if (component && component.props) { // {text:{type:'xxx'},size:{},color:{}}
                     const el = Object.entries(component.props).map(([propName, propConfig]) => {
                         return <ElFormItem label={propConfig.label}>
                             {{
@@ -63,7 +62,7 @@ export default defineComponent({
             return <ElForm labelPosition="top" style='padding:30px'>
                 {content}
                 <ElFormItem>
-                    <ElButton type = "primary" onClick={() => apply()}>应用</ElButton>
+                    <ElButton type="primary" onClick={() => apply()}>应用</ElButton>
                     <ElButton onClick={reset}>重置</ElButton>
                 </ElFormItem>
             </ElForm>
--- src/packages/useCommand.js
+++ src/packages/useCommand.js
@@ -2,7 +2,7 @@ import { before, cloneDeep } from 'lodash'
 import { onUnmounted } from 'vue'
 import { events } from "./events"
 
-export function useCommand(data, foucsData) {
+export function useCommand (data, foucsData) {
     const state = {
         // 前进后退需要指针
         current: -1, // 前进后退的索引
@@ -15,9 +15,9 @@ export function useCommand(data, foucsData) {
     const registry = (command) => {
         state.commandArray.push(command)
         state.commands[command.name] = (...args) => {
-            const { redo,undo } = command.execute(...args)
+            const { redo, undo } = command.execute(...args)
             redo()
-            if(!command.pushQueue) { // 不需要放到队列中直接跳过即可
+            if (!command.pushQueue) { // 不需要放到队列中直接跳过即可
                 return
             }
             let { queue, current } = state
@@ -26,12 +26,12 @@ export function useCommand(data, foucsData) {
              * 如果先放了 组件1 => 组件2 => 撤回 => 组件3
              * 栈里面应该是 组件1 => 组件3
              */
-            if(queue.length > 0) {
+            if (queue.length > 0) {
                 queue = queue.slice(0, current + 1) // 可能在放置过程中有撤销操作，所以根据当前最新的current值来操作
                 state.queue = queue
             }
 
-            queue.push({redo,undo})
+            queue.push({ redo, undo })
             state.current = current + 1
         }
     }
@@ -40,11 +40,11 @@ export function useCommand(data, foucsData) {
     registry({
         name: 'redo',
         keyboard: 'ctrl+y',
-        execute() {
+        execute () {
             return {
-                redo() {
-                    let item = state.queue[state.current+1] // 找到下一步还原操作
-                    if(item) {
+                redo () {
+                    let item = state.queue[state.current + 1] // 找到下一步还原操作
+                    if (item) {
                         item.redo & item.redo()
                         state.current++
                     }
@@ -55,12 +55,12 @@ export function useCommand(data, foucsData) {
     registry({
         name: 'undo',
         keyboard: 'ctrl+z',
-        execute() {
+        execute () {
             return {
-                redo() {
-                    if(state.current == -1) return // 没有可撤销的了
+                redo () {
+                    if (state.current == -1) return // 没有可撤销的了
                     let item = state.queue[state.current] // 找到上一步还原
-                    if(item) {
+                    if (item) {
                         item.undo & item.undo()
                         state.current--
                     }
@@ -71,29 +71,29 @@ export function useCommand(data, foucsData) {
     registry({
         name: 'drag', //如果希望将操作放到队列中可以增加一个属性，标识等会儿操作要放到队列中
         pushQueue: true,
-        init() { // 初始化操作，默认就会执行
+        init () { // 初始化操作，默认就会执行
             this.before = null
             // 监控拖拽开始事件，保存状态
             const start = () => this.before = cloneDeep(data.value.blocks) // 这里进行拷贝，不然会是引用类型
             // 拖拽之后，需要触发对应的指令
             const end = () => state.commands.drag()
             events.on('start', start)
-            events.on('end',end)
+            events.on('end', end)
 
             return () => {
                 events.off(start)
                 events.off(end)
             }
         },
-        execute() {
+        execute () {
             let before = this.before // 拖拽之前的状态
             let after = data.value.blocks // 拖拽之后的状态
             return {
-                redo() { // 拖拽默认一松手 就直接把当前事情做了
-                    data.value = {...data.value, blocks: after}
+                redo () { // 拖拽默认一松手 就直接把当前事情做了
+                    data.value = { ...data.value, blocks: after }
                 },
-                undo() { // 拖拽前一步
-                    data.value = {...data.value, blocks: before}
+                undo () { // 拖拽前一步
+                    data.value = { ...data.value, blocks: before }
                 }
             }
         }
@@ -101,7 +101,7 @@ export function useCommand(data, foucsData) {
     registry({
         name: 'updateContainer', // 更新整个容器
         pushQueue: true,
-        execute(newValue) {
+        execute (newValue) {
             let state = {
                 before: data.value, // 当前值
                 after: newValue // 信值
@@ -119,23 +119,23 @@ export function useCommand(data, foucsData) {
     registry({
         name: 'placeTop',
         pushQueue: true,
-        execute() {
+        execute () {
             let before = cloneDeep(data.value.blocks)
             let after = (() => {
                 // 置顶就在所有的block中找到最大的
                 let { focus, unfocused } = foucsData.value
-                let maxZIndex = unfocused.reduce((prve,block) => {
+                let maxZIndex = unfocused.reduce((prve, block) => {
                     return Math.max(prve, block.zIndex)
                 }, -Infinity)
                 focus.forEach(block => block.zIndex = maxZIndex + 1) // 让当前选中的比最大的+1即可
                 return data.value.blocks
             })()
             return {
-                redo() {
-                    data.value = {...data.value, blocks: after}
+                redo () {
+                    data.value = { ...data.value, blocks: after }
                 },
-                undo() {
-                    data.value = {...data.value, blocks: before}
+                undo () {
+                    data.value = { ...data.value, blocks: before }
                 }
             }
         }
@@ -143,15 +143,15 @@ export function useCommand(data, foucsData) {
     registry({
         name: 'placeBottom',
         pushQueue: true,
-        execute() {
+        execute () {
             let before = cloneDeep(data.value.blocks)
             let after = (() => {
                 let { focus, unfocused } = foucsData.value
-                let minZIndex = unfocused.reduce((prve,block) => {
+                let minZIndex = unfocused.reduce((prve, block) => {
                     return Math.min(prve, block.zIndex)
                 }, Infinity) - 1;
                 // 不能直接-1，因为会出现负值，就看不见了
-                if(minZIndex < 0) {
+                if (minZIndex < 0) {
                     const dur = Math.abs(minZIndex)
                     minZIndex = 0
                     unfocused.forEach(block => block.zIndex += dur)
@@ -160,11 +160,11 @@ export function useCommand(data, foucsData) {
                 return data.value.blocks
             })()
             return {
-                redo() {
-                    data.value = {...data.value, blocks: after}
+                redo () {
+                    data.value = { ...data.value, blocks: after }
                 },
-                undo() {
-                    data.value = {...data.value, blocks: before}
+                undo () {
+                    data.value = { ...data.value, blocks: before }
                 }
             }
         }
@@ -172,17 +172,17 @@ export function useCommand(data, foucsData) {
     registry({
         name: 'delete',
         pushQueue: true,
-        execute() {
+        execute () {
             let state = {
-                before: cloneDeep(data.value.blocks) ,
+                before: cloneDeep(data.value.blocks),
                 after: foucsData.value.unfocused // 选中的都删除了 留下的就是没选中的
             }
             return {
-                redo() {
-                    data.value = {...data.value, blocks: state.after}
+                redo () {
+                    data.value = { ...data.value, blocks: state.after }
                 },
-                undo() {
-                    data.value = {...data.value, blocks: state.before}
+                undo () {
+                    data.value = { ...data.value, blocks: state.before }
                 }
             }
         }
@@ -194,14 +194,14 @@ export function useCommand(data, foucsData) {
             89: 'y'
         }
         const onKeyDown = (e) => {
-            const {ctrlKey, keyCode} = e;
+            const { ctrlKey, keyCode } = e;
             let keyString = []
-            if(ctrlKey) keyString.push('ctrl')
+            if (ctrlKey) keyString.push('ctrl')
             keyString.push(keyCodes[keyCode])
             keyString = keyString.join('+')
-            state.commandArray.forEach(({keyboard, name}) => {
-                if(!keyboard) return // 没有键盘事件
-                if(keyboard === keyString) {
+            state.commandArray.forEach(({ keyboard, name }) => {
+                if (!keyboard) return // 没有键盘事件
+                if (keyboard === keyString) {
                     state.commands[name]();
                     e.preventDefault();
                 }
@@ -220,40 +220,43 @@ export function useCommand(data, foucsData) {
     registry({
         name: 'updateBlock', // 更新某个组件
         pushQueue: true,
-        execute(newBlock, oldBlock) {
+        execute (newBlock, oldBlock) {
+            console.log('新的=',newBlock);
+            console.log('旧的=',oldBlock);
             let state = {
                 before: data.value.blocks,
                 after: (() => {
                     let blocks = [...data.value.blocks] // 拷贝一份用于新的block
                     const index = data.value.blocks.indexOf(oldBlock) // 找到老的 需要通过老的查找
-                    if(index > -1) {
+                    if (index > -1) {
                         blocks.splice(index, 1, newBlock)
                     }
                     return blocks
                 })()
             }
+            console.log('updateBlock=', state);
             return {
-                redo() {
-                    data.value = {...data.value, blocks: state.after}
+                redo: () => {
+                    data.value = { ...data.value, blocks: state.after }
                 },
-                undo() {
-                    data.value = {...data.value, blocks: state.before}
+                undo: () => {
+                    data.value = { ...data.value, blocks: state.before }
                 }
             }
         }
     })
 
-    // 判断哪些有初始化init函数，init函数会返回销毁函数，把销毁函数存起来
-    ;(() => {
-        // 监听键盘事件
-        state.destroyArray.push(keyboardEvent())
+        // 判断哪些有初始化init函数，init函数会返回销毁函数，把销毁函数存起来
+        ; (() => {
+            // 监听键盘事件
+            state.destroyArray.push(keyboardEvent())
 
-        state.commandArray.forEach(command=>command.init && state.destroyArray.push(command.init()))
-    })()
+            state.commandArray.forEach(command => command.init && state.destroyArray.push(command.init()))
+        })()
 
     // 清理绑定的事件
     onUnmounted(() => {
-        state.destroyArray.forEach(fn => fn&&fn())
+        state.destroyArray.forEach(fn => fn && fn())
     })
     return state
 }
